efad07096ba69c6b4b1c7a2c51be1e0720710abf
lujiaqin <2623128580@qq.com>
2024-11-08 16:05:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import index from './views/index.vue';
+</script>
+
+<template>
+  <index></index>
+</template>
+
+<style lang="scss">
+  @use './assets/sass/reset.scss';
+  @use './assets/sass/base.scss';
+</style>
