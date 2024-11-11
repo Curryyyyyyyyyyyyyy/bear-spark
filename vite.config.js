3205e483881c2540cc2a85563a7990e934b9b7f5
lujiaqin <2623128580@qq.com>
2024-11-11 20:20:15 +0800

    环境变量配置

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -6,26 +6,37 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler', // or "modern"
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode,process.cwd())
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler', // or "modern"
+        }
+      },
+    },
+    resolve:{
+      alias:{
+        '@':path.resolve(__dirname, './src')
       }
     },
-  },
-  resolve:{
-    alias:{
-      '@':path.resolve(__dirname, './src')
-    }
+    server: {
+      proxy: {
+        [env.VITE_BASE_URL]: {
+          target: env.VITE_PROXY_DOMAIN,
+          changeOrigin: true,
+        }
+      }
+    },
   }
 })
