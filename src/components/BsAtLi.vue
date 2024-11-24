4804029ba922447fa5a4418502bc6248c8463314
lujiaqin <2623128580@qq.com>
2024-11-24 23:26:32 +0800

    封装了BsEnjoy,BsAtUl,BsAtLi组件

---
+++ src/components/BsAtLi.vue
@@ -0,0 +1,54 @@
+<script setup>
+  const props = defineProps(['avatarUrl','username','fansNum'])
+  const emits = defineEmits(['selectAtUser'])
+  function selectAtUser() {
+    emits('selectAtUser', props.username)
+  }
+</script>
+
+<template>
+  <li class="bs-at-li" @click="selectAtUser">
+    <div class="bs-at-item-avatar">
+      <img :src="props.avatarUrl" alt="">
+    </div>
+    <div class="bs-at-item-info">
+      <p class="info-username">{{ props.username }}</p>
+      <p class="info-stat">{{ props.fansNum }}粉丝</p>
+    </div>
+  </li>
+</template>
+
+<style lang="scss">
+  @use '@/assets/sass/config.scss' as *;
+  .bs-at-li {
+    display: flex;
+    padding: 5px 10px;
+    cursor: pointer;
+    &:hover {
+      background-color: $colorR;
+    }
+    .bs-at-item-avatar {
+      margin-right: 5px;
+      img {
+        height: 40px;
+        width: 40px;
+        border-radius: 20px;
+        vertical-align: middle;
+      }
+    }
+    .bs-at-item-info {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 2px 0;
+      box-sizing: border-box;
+      .info-username {
+        color: $colorB;
+        font-weight: 500;
+      }
+      .info-stat {
+        color: $colorD;
+      }
+    }
+  }
+</style>
---
+++ src/components/BsAtUl.vue
@@ -0,0 +1,58 @@
+<script setup>
+  // import { toRefs } from 'vue';
+
+  // const props = defineProps(['atSelectPosition'])
+  // const { atSelectPosition } = toRefs(props);
+</script>
+
+<template>
+  <div class="bs-at-ul">
+    <div class="bs-at-header">
+      <span>选择或输入你想@的人</span>
+    </div>
+    <ul class="bs-at-list">
+      <p class="bs-at-ul-desc">我的关注</p>
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+  @use '@/assets/sass/config.scss' as *;
+  .bs-at-ul {
+    z-index: 10;
+    position: absolute;
+    bottom: -280px;
+    width: 200px;
+    border: 1px solid #e3e5e7;
+    border-radius: 6px;
+    box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.17);
+    background-color: $colorG;
+    .bs-at-header {
+      height: 40px;
+      display: flex;
+      align-items: center;
+      padding: 0 10px;
+      box-sizing: border-box;
+      color: $colorD;
+    }
+    .bs-at-list {
+      max-height: 225px;
+      overflow-x: hidden;
+      overflow-y: auto;
+      .bs-at-ul-desc {
+        padding: 0 10px;
+        box-sizing: border-box;
+        margin-bottom: 10px;
+        color: $colorC;
+      }
+    }
+    .bs-at-list::-webkit-scrollbar {
+      width: 4px;
+    }
+    .bs-at-list::-webkit-scrollbar-thumb {
+      background-color: $colorF;
+      border-radius: 2px;
+    }
+  }
+</style>
