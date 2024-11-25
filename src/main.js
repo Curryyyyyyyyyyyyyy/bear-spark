2387ea340b3fd58ef10f576ca25fd7dc2139ea3a
lujiaqin <2623128580@qq.com>
2024-11-25 08:45:04 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import App from './App.vue'
 import router from './router'
 import pinia from './store'
 import 'element-plus/dist/index.css'
+import VueLazyLoad from 'vue3-lazyload'
+
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
+app.use(VueLazyLoad, {
+  loading:'/imgs/loading-svg/loading-cylon-red.svg'
+})
 app.mount('#app')
